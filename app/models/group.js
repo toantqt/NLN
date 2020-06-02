e6613daccf96155aaf65e053c9b9e75e29241533
toantqt <toantqt0609@gmail.com>
2020-06-02 21:02:16 +0700

    upload file report

--- app/models/group.js
+++ app/models/group.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const groupSchema = new Schema({
     name: { type: String, require: true},
-    admin: { type: String, require: true}
+    admin: { type: String, require: true},
+    member: [{
+        memberName: {type: String, default: ''}
+    }]
 });
 
 mongoose.model("Group", groupSchema);
